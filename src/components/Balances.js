f99877c8e6537b984551ba8454eddc81b5c26ff1
xiaogit00 <leibingguo@gmail.com>
2022-10-13 17:37:37 +0800

    committed to balances

--- src/components/Balances.js
+++ src/components/Balances.js
@@ -7,6 +7,25 @@ import { dummyData, columns } from '../utils/helper'
 const Balances = ({address, chainId}) => {
   const [data, setData] = useState([])
   const [isLoading, setIsLoading] = useState(true)
+  const apiKey = process.env.REACT_APP_COVALENT_API_KEY
+
+  const getBalancesEndpoint = `https://api.covalenthq.com/v1/${chainId}/address/${address}/balances_v2/`
+
+  const auth = {
+      auth: {
+          username: apiKey
+      }
+  }
+
+  useEffect(() => {
+      axios
+        .get(getBalancesEndpoint, auth)
+        .then(res => {
+            console.log("this is reached")
+            setIsLoading(false)
+            setData(res.data.data.items)
+        })
+  }, [])
 
   return (
       <Table columns={columns} dataSource={data} rowKey='contract_address' style={{width:"70%", margin:"20em"}}/>
